308dff4381978572cc9779b1b1c44785b5a5dba8
Soir <1054632967@qq.com>
2022-05-06 22:16:19 +0800

    IDEA COMMIT AGAIN AGAIN AGAIN

--- src/main/java/com/example/gitdemo/GitTest.java
+++ src/main/java/com/example/gitdemo/GitTest.java
@@ -10,5 +10,6 @@ public class GitTest {
         System.out.println("Hello Git!");
         System.out.println("Hello Git Again!");
         System.out.println("Hello Git Again Again!");
+        System.out.println("Hello Git Again Again Again!");
     }
 }
