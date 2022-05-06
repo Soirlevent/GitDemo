d8a857398a8e1fd4f6d065f7b109d0508d10a3bf
Soir <1054632967@qq.com>
2022-05-06 17:37:58 +0800

    IDEA COMMIT

---
+++ src/main/java/com/example/gitdemo/GitTest.java
@@ -0,0 +1,12 @@
+package com.example.gitdemo;
+
+/**
+ * @Description:
+ * @Author: Soir
+ * @Date: 2022/5/6 17:34
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("Hello Git!");
+    }
+}
